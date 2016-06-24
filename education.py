8392390906b51f494b1c3018bdf603935b0c30dd
Tyler Hedegard <tchedegard@gmail.com>
2016-06-24 21:05:42 +0000

    refactor for style

--- education.py
+++ education.py
@@ -14,20 +14,6 @@ import csv
 import numpy as np
 import statsmodels.api as sm
 
-url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
-
-r = requests.get(url)
-
-soup = BeautifulSoup(r.content)
-
-myTable = soup('table')[6]
-
-my_tbody = myTable('tbody')[3]
-#header = my_tbody('tr')[3]
-max_count = len(my_tbody('tr'))
-
-# Connect to the database
-con = lite.connect('UNdata.db')
 
 def sqlTodf(query):
     """creates a pandas dataframe from a sql query
@@ -38,15 +24,31 @@ def sqlTodf(query):
         cols = [desc[0] for desc in cur.description]
         return pd.DataFrame(rows, columns=cols)
 
+# Collect the UN Education data
+url = "http://web.archive.org/web/20110514112442/\
+       http://unstats.un.org/unsd/demographic/products/socind/education.htm"
+r = requests.get(url)
+soup = BeautifulSoup(r.content)
+myTable = soup('table')[6]
+my_tbody = myTable('tbody')[3]
+max_count = len(my_tbody('tr'))
+
+# Connect to the database
+con = lite.connect('UNdata.db')
+print("Connected to database: UNdata.db")
+
 with con:
     cur = con.cursor()
     # Create the countries table
     cur.execute("DROP TABLE IF EXISTS countries")
     cur.execute("DROP TABLE IF EXISTS gdp")
     cur.execute("CREATE TABLE countries (country text, male number, female number, year number)")
-    cur.execute("CREATE TABLE gdp (country_name text, _1999 text, _2000 text, _2001 text, _2002 text, _2003 text, _2004 text, _2005 text, _2006 text, _2007 text, _2008 text, _2009 text, _2010 text)")
+    cur.execute("CREATE TABLE gdp (country_name text, _1999 text, _2000 text, \
+                _2001 text, _2002 text, _2003 text, _2004 text, _2005 text, \
+                _2006 text, _2007 text, _2008 text, _2009 text, _2010 text)"
+                )
 
-    for i in range(4,max_count): #header is td[3] so this gives all the raw data
+    for i in range(4,max_count):
         row = my_tbody('tr')[i]
         country = (row('td')[0].string)
         male = int(row('td')[7].string)
@@ -71,7 +73,6 @@ for each in list:
     print('  Variance: {}'.format(col.var()))
     
 # Collect GDP Data
-
 with open('GDP.csv','r') as inputFile:
     next(inputFile) # skip the first two lines
     next(inputFile)
@@ -93,7 +94,7 @@ df_gdp = sqlTodf("select * from gdp")
 df_join = sqlTodf("select c.*, g.* from countries c join gdp g on c.country = g.country_name")
 df_join['gdp'] = ''
     
-#Find the gdp for the year of the education data point
+# Find the gdp for the year of the education data point
 for i in range(0,df_join.shape[0]):
     this = '_'+str(df_join['year'][i])
     df_join['gdp'][i] = float(df_join[this][i])
@@ -108,7 +109,7 @@ gdp = df_test['gdp']
 y_male = male
 y_female = female
 # The independent variable is gdp
-# converting to log.  As regular number the fit is even worse
+# Converting GDP to log.  As regular number the fit was even worse
 x = np.log(np.array(gdp, dtype=float))
 X = sm.add_constant(x)
 
@@ -121,11 +122,6 @@ f_female = female_model.fit()
 # Null hypothesis is that the education level and gdp do not relate to each other
 f_male.summary()
 """
-Conclusion: 
-  Reject the null hypothesis since p < 0.05. 
-  Male education levels are impacted by the GPD of the country.
-  The fit of this model is not great though with R-squared = 0.248.
-  f(x) = 0.5437(log(x)) - 0.5170 
                             OLS Regression Results                            
 ==============================================================================
 Dep. Variable:                   male   R-squared:                       0.248
@@ -147,14 +143,15 @@ Prob(Omnibus):                  0.480   Jarque-Bera (JB):                1.077
 Skew:                          -0.189   Prob(JB):                        0.584
 Kurtosis:                       3.193   Cond. No.                         232.
 ==============================================================================
+
+Conclusion: 
+  Reject the null hypothesis since p < 0.05. 
+  Male education levels are impacted by the GPD of the country.
+  The fit of this model is not great though with R-squared = 0.248.
+  f(x) = 0.5437(log(x)) - 0.5170 
 """
 f_female.summary()
 """
-Conclusion: 
-  Reject the null hypothesis since p < 0.05. 
-  Female education levels are impacted by the GPD of the country.
-  The fit of this model is also not great though with R-squared = 0.216.
-  f(x) = 0.6843(log(x)) - 3.7681
                             OLS Regression Results                            
 ==============================================================================
 Dep. Variable:                 female   R-squared:                       0.216
@@ -176,4 +173,10 @@ Prob(Omnibus):                  0.541   Jarque-Bera (JB):                1.326
 Skew:                          -0.197   Prob(JB):                        0.515
 Kurtosis:                       2.742   Cond. No.                         232.
 ==============================================================================
+
+Conclusion: 
+  Reject the null hypothesis since p < 0.05. 
+  Female education levels are impacted by the GPD of the country.
+  The fit of this model is also not great though with R-squared = 0.216.
+  f(x) = 0.6843(log(x)) - 3.7681
 """
